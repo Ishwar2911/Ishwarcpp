1e52f38a45139999d0d4d035a5d1f30e91cae75d
Ishwardeep <ishwar0829@gmail.com>
2025-07-03 12:37:55 +0530

    hello

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// #include<conio.h>
+// using namespace std;
+// int main(){
+//     cout<<"hello cpp";// }
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cout<<"enter a number: ";
+  cin>>num;
+  cout<<"the entered number is: "<<num;
+  return 0;  
+} 
+
